c097b53c43ec963982da67dfc3340339b4179cea
Parva2000 <par.pirooz@gmail.com>
2026-02-25 11:39:13 +0100

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,3 +2,11 @@ project_teams = [["Ava","Samantha","James"],["lucas","zed"],["Edgar","Gabriel"]]
 for team in project_teams:
     for member in team:
         print(member)
+print( )
+sales_data = [[12,17,22],[2,10,3],[5,12,13]]
+scoops_sold = 0
+for item in sales_data :
+    print(item)
+    for element in item:
+        scoops_sold += element
+    print(scoops_sold)
